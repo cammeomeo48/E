27d9da5a62ccb829e1498a675f694e299cfe925b
cammeomeo48 <camcv234112e@st.uel.edu.vn>
2026-01-20 10:35:24 +0700

    hi

---
+++ src/app/classes/student.ts
@@ -0,0 +1,9 @@
+export class Student{
+    constructor(
+        public name:string,
+        public mail:string,
+        public phone:string,
+        public course:string,
+        public time:string
+    ) {}
+}
